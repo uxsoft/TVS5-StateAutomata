ccdc8ac50cbc02ad5942d94155e59b839e28fd1f
Jan Dryk <me@uxsoft.cz>
2016-11-30 21:35:05 +0100

    initial

---
+++ TVS5/AbominationCsvReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVS5
+{
+    public class AbominationCsvReader
+    {
+        public static Automaton Parse(string file)
+        {
+            Automaton automaton = new Automaton();
+
+
+            return automaton;
+        }
+    }
+}
